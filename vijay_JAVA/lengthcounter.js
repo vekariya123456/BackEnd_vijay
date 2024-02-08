438fd38d37c26925a90188dcaa765e061aff7fc5
VijayVekariya <vijayvekariya007@gmail.com>
2024-02-08 09:57:41 +0530

    08-02-2024

--- vijay_JAVA/lengthcounter.js
+++
@@ -1,8 +0,0 @@
-let input = document.getElementById("input");
-let strlength = document.getElementById("strlength");
-
-function lengthcounter()
-{
-    console.log(input.value);
-    strlength.innerHTML = input.value;
-}
--- vijay_JAVA/thispointer.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<stdio.h>
-
-
-class test
-{
-    int bal,amount;
-    
-}
